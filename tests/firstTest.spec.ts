149766dd3007c1140c2b92ece48a1f4ef70a4fc5
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-12-04 20:05:53 -0500

    chore: use different locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,32 +2,30 @@ import { test } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("localhost:4200");
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
 });
 
-test.describe("suite1", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.getByText("Charts").click();
-  });
+test("Locator syntax rules", async ({ page }) => {
+  // By Tag Name
+  await page.locator("input").click();
+  // By ID
+  page.locator("#inputEmail1");
+  // By Class Name
+  page.locator(".shape-rectangle");
+  // By Attribute
+  page.locator('[placeholder="Email"]');
+  // By Class Value (full match)
+  page.locator(
+    '[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]'
+  );
 
-  test("the first test", async ({ page }) => {
-    await page.getByText("Form Layouts").click();
-  });
+  // Combine Selectors
+  page.locator("input[placeholder='Email'][nbinput]");
 
-  test("navigate to datepicker page", async ({ page }) => {
-    await page.getByText("Datepicker").click();
-  });
-});
+  // By Xpath
+  page.locator('//*[@id="inputEmail1"]');
 
-test.describe("suite2", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.getByText("Forms").click();
-  });
-
-  test("the first test1", async ({ page }) => {
-    await page.getByText("Form Layouts").click();
-  });
-
-  test("navigate to datepicker page1", async ({ page }) => {
-    await page.getByText("Datepicker").click();
-  });
+  // By Partial Text Match
+  page.locator(':text-is("Using the Grid")');
 });
