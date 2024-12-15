c6e628d16ef06259b823992051683aa47a652502
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-12-15 16:29:38 -0500

    timeouts

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
   await page.goto("http://uitestingplayground.com/ajax");
   await page.getByText("Button Triggering AJAX Request").click();
+  testInfo.setTimeout(testInfo.timeout * 2000);
 });
 
 test("auto waiting", async ({ page }) => {
@@ -32,4 +33,11 @@ test("alt waits", async ({ page }) => {
 
   const text = await successButton.textContent();
   expect(text).toEqual("Data loaded with AJAX get request.");
+});
+
+test("timeous", async ({ page }) => {
+  test.setTimeout(20000);
+  test.slow();
+  const successButton = page.locator(".bg-success");
+  await successButton.click({ timeout: 20000 });
 });
