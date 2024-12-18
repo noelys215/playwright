e3f56e1cb1ab9a0cdbf49e996591f23ed51ac3ed
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-12-17 19:08:14 -0500

    Lists & Dropdowns

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -83,3 +83,32 @@ test("checkboxes", async ({ page }) => {
     expect(await box.isChecked()).toBeTruthy();
   }
 });
+
+test("lists and dropdowns", async ({ page }) => {
+  const dropDownMenu = page.locator("ngx-header nb-select");
+  await dropDownMenu.click();
+
+  page.getByRole("list");
+  page.getByRole("listitem");
+
+  // const optionList = page.getByRole("list").locator("nb-option");
+  const optionList = page.locator("nb-option-list nb-option");
+  await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"]);
+  await optionList.filter({ hasText: "Cosmic" }).click();
+  const header = page.locator("nb-layout-header");
+  await expect(header).toHaveCSS("background-color", "rgb(50, 50, 89)");
+
+  const colors = {
+    Light: "rgb(255, 255, 255)",
+    Dark: "rgb(34, 43, 69)",
+    Cosmic: "rgb(50, 50, 89)",
+    Corporate: "rgb(255, 255, 255)",
+  };
+
+  await dropDownMenu.click();
+  for (const color in colors) {
+    await optionList.filter({ hasText: color }).click();
+    await expect(header).toHaveCSS("background-color", colors[color]);
+    if (color !== "Corporate") await dropDownMenu.click();
+  }
+});
