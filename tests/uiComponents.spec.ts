a9ff14b540adaffd56a6bbdaf0fc3150cdde7a04
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-12-22 11:47:12 -0500

    sliders

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -232,3 +232,34 @@ test("date picker", async ({ page }) => {
 
   await expect(calendarInputField).toHaveValue(dateToAssert);
 });
+
+test("sliders", async ({ page }) => {
+  /* Update attribute */
+
+  /*
+    const tempGauge = page.locator(
+      "[tabtitle='Temperature'] ngx-temperature-dragger circle"
+    );
+    await tempGauge.evaluate((node) => {
+      node.setAttribute("cx", "232.630");
+      node.setAttribute("cy", "232.630");
+    });
+    await tempGauge.click();
+  */
+
+  /* Mouse movement */
+
+  const tempBox = page.locator(
+    "[tabtitle='Temperature'] ngx-temperature-dragger"
+  );
+  await tempBox.scrollIntoViewIfNeeded();
+  const box = await tempBox.boundingBox();
+  const x = box.x + box.width / 2;
+  const y = box.y + box.height / 2;
+  await page.mouse.move(x, y);
+  await page.mouse.down();
+  await page.mouse.move(x + 100, y);
+  await page.mouse.move(x + 100, y + 100);
+  await page.mouse.up();
+  await expect(tempBox).toContainText("30");
+});
