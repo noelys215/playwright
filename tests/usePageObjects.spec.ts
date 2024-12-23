a49f53941a052ccd43403f5f4f485e147c6b0f10
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-12-23 10:40:33 -0500

    Page object model

---
+++ tests/usePageObjects.spec.ts
@@ -0,0 +1,11 @@
+import { test } from "@playwright/test";
+import { NavigationPage } from "../page-objects/navigationPage";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("localhost:4200");
+});
+
+test("navigate to form page", async ({ page }) => {
+  const navigateTo = new NavigationPage(page);
+  await navigateTo.formLayoutsPage();
+});
