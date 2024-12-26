7706d66552e4269fbebe19fea96810712f7d28f3
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-12-26 11:20:37 -0500

    parameterized methods

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test } from "@playwright/test";
 import { NavigationPage } from "../page-objects/navigationPage";
+import { FormLayoutsPage } from "../page-objects/formLayoutsPage";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("localhost:4200");
@@ -12,4 +13,22 @@ test("navigate to form page", async ({ page }) => {
   await navigateTo.smartTablePage();
   await navigateTo.toastrPage();
   await navigateTo.tooltipPage();
+});
+
+test("parameterized methods", async ({ page }) => {
+  const navigateTo = new NavigationPage(page);
+  const onFormLayoutsPage = new FormLayoutsPage(page);
+
+  await navigateTo.formLayoutsPage();
+  await onFormLayoutsPage.submitUsingTheGridFormUsingCredentialsAndSelectOption(
+    "test@test.com",
+    "Welcome1",
+    "Option 1"
+  );
+
+  await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox(
+    "John Smith",
+    "john@test.com",
+    true
+  );
 });
